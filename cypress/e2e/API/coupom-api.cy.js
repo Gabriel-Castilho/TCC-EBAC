c01fa0a729532217648bb1ab58acac126021abcb
Gabriel Castilho <gabriel.castilho@outlook.com.br>
2023-05-07 13:43:01 -0300

    removendo contratos e adicionando comando de script

--- cypress/e2e/API/coupom-api.cy.js
+++ cypress/e2e/API/coupom-api.cy.js
@@ -1,21 +1,15 @@
 /// <reference types="cypress" />
-//import coupom from '../../contracts/coupons.contract'
 
 describe ('Teste da API de cupom',()=>{
 
     let basicAuth ='Basic YWRtaW5fZWJhYzpAYWRtaW4hJmJAYyEyMDIy'
     let nameFixed = Math.floor(Math.random()*1000)
-  /*  it('Deve validar o contrato de cupoms',()=>{
-        cy.request('/rest-api/docs/#/coupons/').then(response=>{
-            return coupom.validateAsync(response.body)
-        })
-    })
-    */
+
+    
 
     it('Deve listar todos os cupons',()=>{
         cy.listCoupons(basicAuth).then(response=>{
             expect(response.status).to.equal(200)
-            expect(response.body[0].code).to.contain("cupom")
         })
     })
 
@@ -47,7 +41,7 @@ describe ('Teste da API de cupom',()=>{
         let discount = ["fixed_product","percent","fixed_cart"]
         let discountSort = Math.floor(Math.random() * (3-0)+0)
         let quantityDiscount = Math.floor(Math.random()* 100)
-        cy.insertCoupon(basicAuth,`${nameFixed}`,`${quantityDiscount}`,`${discount[discountSort]}`,`Desconto de ${quantityDiscount} ${discount[discountSort]}`)
+        cy.insertCoupon(basicAuth,"cupom20121",`${quantityDiscount}`,`${discount[discountSort]}`,`Desconto de ${quantityDiscount} ${discount[discountSort]}`)
         .then(response=>{
             expect(response.status).to.equal(400)
             expect(response.body.message).to.equal("O código de cupom já existe")
@@ -58,7 +52,7 @@ describe ('Teste da API de cupom',()=>{
         let discount = ["fixed_product","percent","fixed_cart"]
         let discountSort = Math.floor(Math.random() * (3-0)+0)
         let quantityDiscount = Math.floor(Math.random()* 100)
-        cy.insertCoupon("cupom20121",`${quantityDiscount}`,`${discount[discountSort]}`,`Desconto de ${quantityDiscount} ${discount[discountSort]}`)
+        cy.insertCoupon("","cupom20121",`${quantityDiscount}`,`${discount[discountSort]}`,`Desconto de ${quantityDiscount} ${discount[discountSort]}`)
         .then(response=>{
             expect(response.status).to.equal(401)
             expect(response.body.message).to.equal("Desculpe, você não possui permissão para criar recursos.")
